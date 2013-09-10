c6e31f8d9cf837914314ae3daf4aa13838c5867e
Leandro Lovisolo <leandro@leandro.me>
2013-09-10 11:32:45 -0300

    Problema 1

--- p1/client.c
+++ p1/client.c
@@ -2,4 +2,30 @@
 
 int main(int argc, char *argv[])
 {
+	if(argc != 2) {
+		printf("Uso: %s <dirección IP>\n", argv[0]);
+		return -1;
+	}
+
+	struct sockaddr_in sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(PORT);
+
+	if(inet_aton(argv[1], &sin.sin_addr) == 0) {
+		printf("IP inválido.\n");
+		return -1;
+	}
+
+	int s = socket(AF_INET, SOCK_DGRAM, 0);
+
+	for(;;) {
+		char buf[1024];
+		printf("> ");
+		fgets(buf, sizeof(buf), stdin);
+		sendto(s, buf, strlen(buf) + 1, 0,
+			(const struct sockaddr *) &sin, sizeof(sin));
+	}
+
+	close(s);
+
 }
